73e17e55f80c7a90c64039a6a7705575a4caab22
104541862 <104541862@student.swin.edu.au>
2026-04-24 21:55:38 +1000

    update uninformed

--- Spike 4/bots/Uninformed.py
+++ Spike 4/bots/Uninformed.py
@@ -17,32 +17,22 @@ class Uninformed(object):
         # Don't send ships if there are less than 10
         if src.ships <= 10:
             return
-
         # Simply expand to the nearest neutral planet
         if neutral_planets:
             # Expand safely to the closest neutral planet
             dest = min(neutral_planets.values(), key=lambda p: src.distance_to(p))
-
             send_amount = int(src.ships * 0.5)
-
         # Attack enemies afterwards and only the weakest ones
         elif enemy_planets:
-            dest = min(
-                enemy_planets.values(),
-                key=lambda p: p.ships
-            )
+            dest = min(enemy_planets.values(), key=lambda p: p.ships)
 
             # Only attack if we are much stronger
             if src.ships <= dest.ships * 1.5:
                 return
-
             send_amount = int(src.ships * 0.4)
-
         else:
             return
-
         # Minimum amount, just in case
         send_amount = min(send_amount, src.ships - 5)
-
         if send_amount > 0:
             gameinfo.planet_order(src, dest, send_amount)
